c9371ff05348d3a2ff5df576d7153ea82246ff8f
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-01-15 17:18:44 -0600

    Agregar definición de tabla 'Clients' y datos de siembra en la base de datos

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,21 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
+
+// Define your tables and their schema here.
+// Read more: https://astro.build/db
+
+const Clients = defineTable({
+  columns: {
+    id: column.number({primaryKey: true}),
+    name: column.text(),
+    age: column.number(),
+    isActive: column.boolean()          
+  }
+});
+
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    Clients
+  }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,14 @@
-import { db } from 'astro:db';
+import { Clients, db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	await db.insert(Clients).values([
+		{ id: 1, name: "Kasim", age: 30, isActive: true },
+		{ id: 2, name: "Jose", age: 31, isActive: true },
+		{ id: 3, name: "John", age: 32, isActive: true },
+		{ id: 4, name: "Jane", age: 33, isActive: false },
+		{ id: 5, name: "Bob", age: 34, isActive: true },
+	  ]);
+
+	console.log('Seeding database...');
 }
