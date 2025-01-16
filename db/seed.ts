c31d0e2920aecb67f5cd91b003841f3d00877bed
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-01-16 05:35:45 -0600

    Eliminar registro de usuario "Bob" del script de siembra de la base de datos

--- db/seed.ts
+++ db/seed.ts
@@ -7,7 +7,6 @@ export default async function seed() {
 		{ id: 2, name: "Jose", age: 31, isActive: true },
 		{ id: 3, name: "John", age: 32, isActive: true },
 		{ id: 4, name: "Jane", age: 33, isActive: false },
-		{ id: 5, name: "Bob", age: 34, isActive: true },
 	  ]);
 
 	console.log('Seeding database...');
