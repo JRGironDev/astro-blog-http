7eaf6163b7af3e792ebefd27d5341a2b4411e12d
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-01-16 08:01:39 -0600

    Agregar dependencia de '@astrojs/studio' versión 0.1.3 y eliminar registro de usuario "Jane" del script de siembra

--- db/seed.ts
+++ db/seed.ts
@@ -6,7 +6,6 @@ export default async function seed() {
 		{ id: 1, name: "Kasim", age: 30, isActive: true },
 		{ id: 2, name: "Jose", age: 31, isActive: true },
 		{ id: 3, name: "John", age: 32, isActive: true },
-		{ id: 4, name: "Jane", age: 33, isActive: false },
 	  ]);
 
 	console.log('Seeding database...');
