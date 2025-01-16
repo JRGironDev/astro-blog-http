a0672f2cf8b35f8c7257845d256f685ce29b9ee0
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-01-16 03:42:12 -0600

    Agregar manejo de solicitudes PATCH y DELETE en la API de 'clients'

--- src/pages/api/clients/[clientId].ts
+++ src/pages/api/clients/[clientId].ts
@@ -1,6 +1,7 @@
 export const prerender = false;
 
 import type { APIRoute } from "astro";
+import { Clients, db, eq } from "astro:db";
 
 export const GET: APIRoute = async ({ params, request }) => {
 
@@ -15,23 +16,35 @@ export const GET: APIRoute = async ({ params, request }) => {
 };
 
 export const PATCH: APIRoute = async ({ params, request }) => {
-    const clientId = params.clientId;
+    const clientId = params.clientId ?? '';
 
-    const body  =  {
-        method: 'PATCH',
-        clientId: clientId
+    try {
+        const { id, ...body } = await request.json();
+
+        // Update xxx=xxx in the table
+        const results = await db.update(Clients).set(body)
+            .where(eq(Clients.id, +clientId));
+        
+        const updatedClient = await db.select().from(Clients).where(eq(Clients.id, +clientId));
+
+        return new Response(JSON.stringify(
+            updatedClient.at(0),
+        ), {status: 201, headers: {'Content-Type': 'application/json'} });
+
+    } catch (error) {
+        console.log(error);
+        return new Response(JSON.stringify(error), {status: 400, headers: {'Content-Type': 'application/json'} });
     }
-
-    return new Response(JSON.stringify(body), {status: 200, headers: {'Content-Type': 'application/json'} });
 };
 
 export const DELETE: APIRoute = async ({ params, request }) => {
-    const clientId = params.clientId;
+    const clientId = params.clientId ?? '';
 
-    const body  =  {
-        method: 'DELETE',
-        clientId: clientId
+    const { rowsAffected} = await db.delete(Clients).where(eq(Clients.id, +clientId));
+    
+    if (rowsAffected > 0) {
+        return new Response(JSON.stringify({msg: 'Client deleted'}), {status: 200, headers: {'Content-Type': 'application/json'} });
     }
 
-    return new Response(JSON.stringify(body), {status: 200, headers: {'Content-Type': 'application/json'} });
+    return new Response(JSON.stringify({msg: 'Client not deleted'}), {status: 400, headers: {'Content-Type': 'application/json'} });
 };
